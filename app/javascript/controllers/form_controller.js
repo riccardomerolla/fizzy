3e5b531d12e8ca9b06317fe5256738bf8f989b9a
Jason Zimdars <jz@37signals.com>
2025-02-13 17:40:18 -0600

    Extract into a single-purpose controller

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -11,31 +11,6 @@ export default class extends Controller {
     this.cancelTarget?.click()
   }
 
-  pasteFiles(event) {
-    const editor = event.target.closest("house-md")
-    if (!editor) return
-    
-    const files = event.clipboardData?.files
-    if (!files?.length) return
-
-    event.preventDefault()
-
-    for (const file of files) {
-      const uploadEvent = new CustomEvent("house-md:before-upload", { 
-        bubbles: true, 
-        detail: { file },
-        cancelable: true 
-      })
-      
-      if (editor.dispatchEvent(uploadEvent)) {
-        const upload = document.createElement("house-md-upload")
-        upload.file = file
-        upload.uploadsURL = editor.dataset.uploadsUrl
-        editor.appendChild(upload)
-      }
-    }
-  }
-
   preventAttachment(event) {
     event.preventDefault()
   }
--- app/javascript/controllers/remote_auto_save_controller.js
+++ app/javascript/controllers/remote_auto_save_controller.js
@@ -10,26 +10,4 @@ export default class extends Controller {
   submit() {
     this.autoSaveOutlet.submit()
   }
-
-  pasteFiles(event) {
-    const files = event.clipboardData?.files
-    if (!files?.length) return
-    
-    const editor = this.element.querySelector('house-md')
-    if (!editor) return
-
-    for (const file of files) {
-      const upload = new CustomEvent('house-md:before-upload', {
-        bubbles: true,
-        detail: { file }
-      })
-      
-      if (editor.dispatchEvent(upload)) {
-        const uploadElement = document.createElement('house-md-upload')
-        uploadElement.file = file
-        uploadElement.uploadsURL = editor.dataset.uploadsUrl
-        editor.appendChild(uploadElement)
-      }
-    }
-  }
 }
