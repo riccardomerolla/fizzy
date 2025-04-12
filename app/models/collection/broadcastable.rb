a579e67057662d9fa2c0ef546eab532283fa1f7e
David Heinemeier Hansson <david@hey.com>
2025-04-12 20:08:42 +0200

    We are not using the parameter

--- app/models/collection/broadcastable.rb
+++ app/models/collection/broadcastable.rb
@@ -3,6 +3,6 @@ module Collection::Broadcastable
 
   included do
     broadcasts_refreshes
-    broadcasts_refreshes_to ->(collection) { :collections }
+    broadcasts_refreshes_to ->(_) { :collections }
   end
 end
